c588174ced083ecc929ec1ab89fe5c42f336392b
Jean Y <fyacar_14@hotmail.com>
2022-03-13 16:34:04 -0500

    correccion errores 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 
-// minuto 13
-
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListInput = document.querySelector('[data-new-list-input]');
@@ -8,7 +6,7 @@ const newListInput = document.querySelector('[data-new-list-input]');
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_LIST_ID_KEY = 'task.selectedListID';
 const LOCAL_STORAGE_URL_FONDO = 'task.urlFondo';
-//lists lee las listas desde el local storage y almacena esa información
+
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_LIST_ID_KEY);
 
@@ -29,7 +27,6 @@ const newTaskInput = document.querySelector('[data-new-task-input]');
 
 const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]');
 
-//
 const contenedorListas = document.querySelector('.lista'); 
 
 const btnUrlFondo = document.querySelector('[data-btn-mostar-input-cambiar-fondo]');
@@ -41,37 +38,10 @@ const contenedorCambiarFondo = document.querySelector('.cambiar-fondo');
 let contador = 1;
 
 
-const aaa = document.getElementById('aaa');
+///// GENERAL ///////
 
-/* aaa.addEventListener('click' , e=>{
-  console.log("valor de urlFondo: "+ urlFondo);
-  console.log(localStorage.getItem(LOCAL_STORAGE_URL_FONDO));
-}) */
 
-btnMostrarInputCambiarFondo.addEventListener('click' , e=>{  
-  if(contador%2!==0){   
-    contenedorCambiarFondo.style.display = 'flex';
-    contador ++;
-  } else{    
-    contenedorCambiarFondo.style.display = 'none';
-    contador ++;
-  }
-})
-
-btnAgregarFondo.addEventListener('click', e=>{  
-  e.preventDefault();
-  url = document.getElementById('url-fondo').value;
-  localStorage.setItem(LOCAL_STORAGE_URL_FONDO, url);  
-  location.reload(); 
-})
-
-btnEliminarFondo.addEventListener('click',e=>{
-  e.preventDefault();  
-  localStorage.setItem(LOCAL_STORAGE_URL_FONDO, 'https://source.unsplash.com/collection/3416281');  
-  /* 3416281 */
-  /* localStorage.setItem(LOCAL_STORAGE_URL_FONDO, 'https://images.unsplash.com/photo-1521208916306-71fce562015a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80');   */
-  location.reload();  
-})
+render();
 
 function saveAndRender(){
   save();
@@ -100,34 +70,16 @@ function render() {
     clearElement(taskContainer);
     renderTasks(selectedList);
   }  
-  let contador = 1;  
+  contador = 1;  
 }
 
-clearCompleteTasksButton.addEventListener('click', e=>{
-  const selectedList = lists.find(list=> list.id === selectedListId);
-  selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
-  saveAndRender();
-
-})
-
-taskContainer.addEventListener('click', e=>{
-  if(e.target.tagName.toLowerCase()=== 'input'){
-    const selectedList = lists.find(list => list.id === selectedListId);
-    const selectedTask = selectedList.tasks.find(task=> task.id === e.target.id);
-    selectedTask.complete = e.target.checked;
-    save();
-    renderTaskCount(selectedList);
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
   }
-})
+}
 
-
-deleteListButton.addEventListener('click', e => {
-  //creamos una nueva lista filtrando la lista que está seleccionada actualmente
-  lists = lists.filter(list => list.id !== selectedListId);
-  //le damos el valor de null a selectedListID para que no guarde el último id.
-  selectedListId = null;
-  saveAndRender();
-})
+///////// LISTAS ////////////////
 
 newListForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -138,6 +90,46 @@ newListForm.addEventListener('submit', e => {
   lists.push(list);
   saveAndRender(); 
 })
+
+function createList(name){
+  return {
+   id: Date.now().toString(),
+   name: name,
+   tasks:[]
+  }
+
+}
+
+function renderLists(){
+  lists.forEach(list => {
+    const listElement = document.createElement('li');
+    listElement.dataset.listId=list.id;
+    listElement.classList.add('nombre-lista');
+    listElement.innerText = list.name;
+    // Si el id del elemento es igual al id al que se ha hecho click (listsContaine.addEventListener()) se le agrega la clase 'Lista Activa'
+    if(list.id === selectedListId) listElement.classList.add('lista-activa');
+    listsContainer.appendChild(listElement);
+    //   
+  })
+}
+
+deleteListButton.addEventListener('click', e => {
+  //creamos una nueva lista filtrando la lista que está seleccionada actualmente
+  lists = lists.filter(list => list.id !== selectedListId);
+  //le damos el valor de null a selectedListID para que no guarde el último id.
+  selectedListId = null;
+  saveAndRender();
+})
+
+listsContainer.addEventListener('click', e =>{
+  if(e.target.tagName.toLowerCase() === 'li'){
+    // Se guarda el id del elemento li al que le ha dado click
+    selectedListId = e.target.dataset.listId;    
+    saveAndRender();
+  }
+} )
+
+///////// TAREAS ////////////////
 
 newTaskForm.addEventListener('submit', e => {
   e.preventDefault();
@@ -158,15 +150,6 @@ function createTask(name){
    }
 }
 
-function createList(name){
-  return {
-   id: Date.now().toString(),
-   name: name,
-   tasks:[]
-  }
-
-}
-
 function renderTasks(selectedList){
     selectedList.tasks.forEach(task =>{
       const taskElement = document.importNode(taksTemplate.content , true);
@@ -180,40 +163,53 @@ function renderTasks(selectedList){
     })
 }
 
-
-
 function renderTaskCount(selectedList){
     const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length;
     const taskString = incompleteTaskCount === 1 ? "tarea" : "tareas";
     listCountElement.innerText= `${incompleteTaskCount} ${taskString} por completar`
 }
 
-function renderLists(){
-  lists.forEach(list => {
-    const listElement = document.createElement('li');
-    listElement.dataset.listId=list.id;
-    listElement.classList.add('nombre-lista');
-    listElement.innerText = list.name;
-    // Si el id del elemento es igual al id al que se ha hecho click (listsContaine.addEventListener()) se le agrega la clase 'Lista Activa'
-    if(list.id === selectedListId) listElement.classList.add('lista-activa');
-    listsContainer.appendChild(listElement);
-    //   
-  })
-}
-
-function clearElement(element) {
-  while (element.firstChild) {
-    element.removeChild(element.firstChild);
+taskContainer.addEventListener('click', e=>{
+  if(e.target.tagName.toLowerCase()=== 'input'){
+    const selectedList = lists.find(list => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find(task=> task.id === e.target.id);
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
   }
-}
+})
 
 
-listsContainer.addEventListener('click', e =>{
-  if(e.target.tagName.toLowerCase() === 'li'){
-    // Se guarda el id del elemento li al que le ha hecho click
-    selectedListId = e.target.dataset.listId;    
-    saveAndRender();
+clearCompleteTasksButton.addEventListener('click', e=>{
+  const selectedList = lists.find(list=> list.id === selectedListId);
+  selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
+  saveAndRender();
+
+})
+
+//////// FONDO //////////////
+
+btnMostrarInputCambiarFondo.addEventListener('click' , e=>{  
+  if(contador%2!==0){   
+    contenedorCambiarFondo.style.display = 'flex';
+    contador ++;
+  } else{    
+    contenedorCambiarFondo.style.display = 'none';
+    contador ++;
   }
-} )
+})
 
-render();
+btnAgregarFondo.addEventListener('click', e=>{  
+  e.preventDefault();
+  url = document.getElementById('url-fondo').value;
+  localStorage.setItem(LOCAL_STORAGE_URL_FONDO, url);  
+  location.reload(); 
+})
+
+btnEliminarFondo.addEventListener('click',e=>{
+  e.preventDefault();  
+  localStorage.setItem(LOCAL_STORAGE_URL_FONDO, 'https://source.unsplash.com/collection/3416281'); 
+  location.reload();  
+})
+
+
